c03f23d8ea7da6e60f534f451c0123eed91a0f01
Fairy Shaw <fkxiaowenhui@163.com>
2014-11-22 20:03:59 +0800

    aaa

--- js/myhome.js
+++ js/myhome.js
@@ -1,9 +1,9 @@
 $("li.course-list-item").click(function() {
 	$("li.course-list-item").css("background-color", "#34495e");
 	$(this).css("background", "transparent");
-	$("h4.course-name").click(function() {
-	    $("h4.course-name").css("color", "#fff");
-	    $(this).css("color", "#1abc9c");
+	$("h4.course-name").css("color", "#fff");
+	$("course-list-item option:selected").each(function() {
+		$(this > "h4.course-name").css("color", "#34495e");
 	});
 });
 
