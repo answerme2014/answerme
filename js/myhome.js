ff3770e70a4993b3818bfad7e9c2910940d6be71
Fairy Shaw <fkxiaowenhui@163.com>
2014-11-27 21:33:18 +0800

    修改加载的课程页面，添加课程页面点赞的js

--- js/myhome.js
+++ js/myhome.js
@@ -1,12 +1,28 @@
-$("li.course-list-item").click(function() {
-	$("li.course-list-item").css("background-color", "#34495e");
-	$(this).css("background", "transparent");
-	$("h4.course-name").css("color", "#fff");
-	$(this).find("h4").css("color", "#19B68A");
-	$("div.homework").find("ul.homework-list").css("display", "none");
-	$("div.course-page").css("display", "inherit");
-});
+$(document).ready(function() {
+	$("li.course-list-item").click(function() {
+		$("li.course-list-item").css("background-color", "#34495e");
+		$(this).css("background", "transparent");
+		$("h4.course-name").css("color", "#fff");
+		$(this).find("h4").css("color", "#19B68A");
+		$("div.homework").find("ul.homework-list").css("display", "none");
+		$("div.course-page").css("display", "inherit");
+	});
 
-$("span.finish-bt").click(function() {
-	$(this).parent().parent().remove();
+	$("span.finish-bt").click(function() {
+		$(this).parent().parent().remove();
+	});
+
+	$("#good-img").click(function() {
+		var a = $("#good").text();
+		var sum = 0;
+		var j = 1;
+		for ( var i = a.length - 1; i >= 0; i--) {
+			sum += a[i] * j;
+			j *= 10;
+		}
+		sum += 1;
+		$("#good").text(sum);
+		$("#good-img").css("color", "#2c3e50");
+	});
+
 });
