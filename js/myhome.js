7a2a5372884470f9c423e555cc99fe82d0a8b390
Fairy Shaw <fkxiaowenhui@163.com>
2014-11-27 19:16:57 +0800

    js做修改

--- js/myhome.js
+++ js/myhome.js
@@ -2,11 +2,11 @@ $("li.course-list-item").click(function() {
 	$("li.course-list-item").css("background-color", "#34495e");
 	$(this).css("background", "transparent");
 	$("h4.course-name").css("color", "#fff");
-	$("course-list-item option:selected").each(function() {
-		$(this > "h4.course-name").css("color", "#34495e");
-	});
+	$(this).find("h4").css("color", "#19B68A");
+	$("div.homework").find("ul.homework-list").css("display", "none");
+	$("div.course-page").css("display", "inherit");
 });
 
 $("span.finish-bt").click(function() {
-	$(this).remove();
+	$(this).parent().parent().remove();
 });
